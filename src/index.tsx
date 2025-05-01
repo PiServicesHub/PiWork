f678d4b1dae435aabd26b9d40e2aa77be64015b3
PiServicesHub <scorpius1291@gmail.com>
2025-05-01 12:22:16 +0200

    fixed html

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Deine Hauptkomponente (App)
 import './styles.css'; // Deine CSS-Datei
+import './index.css';
+
 
 // Die App wird in das "root"-Element der HTML-Datei gerendert
 const rootElement = document.getElementById('root') as HTMLElement;
