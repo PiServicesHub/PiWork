1b03482daf72e36214655a02aca0f5850c6eda3a
PiServicesHub <scorpius1291@gmail.com>
2025-05-01 02:34:06 +0200

    Updates

---
+++ precache-manifest.d8941ccd6e6ee931b1d55a2ffc1387fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f94f30347d25fe5d96538d69f9532b5",
+    "url": "/PiWork/index.html"
+  },
+  {
+    "revision": "a7be8d8c2db2933ef398",
+    "url": "/PiWork/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "1d76356834810f764ae5",
+    "url": "/PiWork/static/css/main.0ffa0c7f.chunk.css"
+  },
+  {
+    "revision": "a7be8d8c2db2933ef398",
+    "url": "/PiWork/static/js/2.e5a10ec2.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/PiWork/static/js/2.e5a10ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d76356834810f764ae5",
+    "url": "/PiWork/static/js/main.a71a9fb5.chunk.js"
+  },
+  {
+    "revision": "3481f1dc67301d2d55ee",
+    "url": "/PiWork/static/js/runtime-main.d434435f.js"
+  }
+]);
