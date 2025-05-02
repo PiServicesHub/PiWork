b4d02931184a4d7d2951b6d44095ad9f660b2d06
PiServicesHub <scorpius1291@gmail.com>
2025-05-02 06:13:35 +0200

    Updates

--- precache-manifest.d65bc1e2e340014056929d2eb491d887.js
+++ precache-manifest.d65bc1e2e340014056929d2eb491d887.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b048359dc288acf9ba74078d30477fc",
+    "revision": "7bb59f5a62d90c59c3c74cd73cae6ce2",
     "url": "./index.html"
   },
   {
-    "revision": "0496c6a9bfad94c5a9d4",
-    "url": "./static/css/main.8f737126.chunk.css"
+    "revision": "821cb800159d2140fae5",
+    "url": "./static/css/main.65e65295.chunk.css"
   },
   {
     "revision": "66cf4d69d4261051774a",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.80cde81b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0496c6a9bfad94c5a9d4",
+    "revision": "821cb800159d2140fae5",
     "url": "./static/js/main.a1389332.chunk.js"
   },
   {
